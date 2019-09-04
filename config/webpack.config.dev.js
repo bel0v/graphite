d21e32f3c7d93843b0e2ff322607fd8e0e9dbc68
Egor Belov <georgy.belov@revolut.com>
2019-09-04 18:47:59 +0300

    init

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './lib/index.js',
+  output: {
+    filename: 'graphite.js',
+    path: path.resolve(__dirname, '../dist'),
+    library: 'graphite',
+    libraryExport: 'default',
+    libraryTarget: 'var'
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: path.join(__dirname, '../examples'),
+    compress: false,
+    port: 9000
+  }
+};
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: './lib/index.js',
+  output: {
+    filename: 'graphite.min.js',
+    path: path.resolve(__dirname, '../dist'),
+    library: 'graphite',
+    libraryExport: 'default',
+    libraryTarget: 'var'
+  },
+};
